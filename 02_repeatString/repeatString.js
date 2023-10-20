ef8463c49dd4578518e2593ab9b8c5a9de36c145
mantillonv <marceloantillon93@gmail.com>
2023-10-19 18:21:19 -0700

    Solved exercises 01_07

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+const repeatString = function repeatString(str, num) {
+    str = 'hey';
+    if (num > 0) {
+        return str.repeat(num);
+    } else if (num < 0) {
+        return 'ERROR';
+    } else {
+        return '';
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
+
+/* My solution differed from the one provided by the exercises, getting stuck in the last test after passing the rest.
+I should have used a loop but instead focused on conditionals */
